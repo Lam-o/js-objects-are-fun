67e3d3f67938f786995c392f3aab0464609b08a4
lam <i.mailed.lam@gmail.com>
2023-12-24 20:55:01 +0200

    OBjects are fun

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+
+//new variable with empty object
+const learning = {};
+
+learning.topic = "JS";
+
+learning.learningGoals = [
+    "Memorize all the syntax",
+    "More time",
+    "Create great code",
+    "Write cool apps"
+];
+learning.category = "front end development";
+learning.topicImportance = "high";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1,1);
+
+console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}.`;
+topicElement.remove("hide");
+
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
+
+
+
+
+
+
+
+
+
+
+
+
+
